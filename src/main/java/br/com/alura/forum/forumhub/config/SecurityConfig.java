257dae3a09736cd9c002e531c2524306ceade1a1
LaylAgnes <laylaagnes001@gmail.com>
2026-02-05 22:06:18 -0300

    Atualização do projeto

--- src/main/java/br/com/alura/forum/forumhub/config/SecurityConfig.java
+++ src/main/java/br/com/alura/forum/forumhub/config/SecurityConfig.java
@@ -16,12 +16,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
-                )
-                .httpBasic();
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated()
+        );
+        http.httpBasic();
 
         return http.build();
     }
